e3de1ce181a1620c89817ca332620b2cd00e285e
Naveen <naveen.balasubramaniam@mallow-tech.com>
2024-01-20 16:36:13 +0530

    Commentd invoice mail command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('send:invoice-to-mail 5')->everyFiveMinutes();
+//        $schedule->command('send:invoice-to-mail 5')->everyFiveMinutes();
     }
 
     /**
